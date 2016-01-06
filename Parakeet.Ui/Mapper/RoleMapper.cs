239143282058f6715ade99e4aa7f48341e58fe58
Alex <alexsuttmiller@omniaery.com>
2016-01-05 21:47:03 -0600

    More support for Roles in the data layer, and UI layer

---
+++ Parakeet.Ui/Mapper/RoleMapper.cs
@@ -0,0 +1,15 @@
+﻿namespace Parakeet.Ui.Mapper
+{
+    public static class RoleeMapper
+    {
+        public static Data.Entities.Role ToEntity(this Model.Role role)
+        {
+            return new Data.Entities.Role
+            {
+                Id = role.Id,
+                Description = role.Description,
+                Name = role.Name
+            };
+        }
+    }
+}
---
+++ Parakeet.Ui/Model/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace Parakeet.Ui.Model
+{
+    public class Role
+    {
+        public long? Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
